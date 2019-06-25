1105ef19af64525199c1f692442110bbfb448e6c
Sunayana Amuru <48981024+sunayanareddyAmuru@users.noreply.github.com>
2019-06-25 13:06:17 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -141,7 +141,7 @@ createRestaurantHTML = (restaurant) => {
 	const li = document.createElement('li');
 	const image = document.createElement('img');
 	image.className = 'restaurant-img';
-	image.src = DBHelper.imageUrlForRestaurant(restaurant);
+	image.src = "."+DBHelper.imageUrlForRestaurant(restaurant);
 	image.alt = restaurant.name;
 	li.append(image);
 	const name = document.createElement('h1');
